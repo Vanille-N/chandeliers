1c756d057784c8c980c500c60924514b645e820e
Vanille-N <neven.villani@crans.org>
2023-10-12 20:48:53 +0200

    tests

---
+++ chandeliers-lus/tests/fail/ui/syn/unclosed.rs
@@ -0,0 +1,8 @@
+chandeliers_lus::decl! {
+    node foo() returns ();
+    let
+        x = 9;
+        y = 2;
+}
+
+fn main() {}
---
+++ chandeliers-lus/tests/pass/ui/assign.rs
@@ -0,0 +1,18 @@
+#![allow(unused_variables)]
+// Increasingly weird but very valid assignment tuples.
+chandeliers_lus::decl! {
+    node test() returns ();
+    var a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q : int;
+    let
+        a = 1;
+        (b) = 2;
+        ((c)) = 3;
+        (d, (e), f) = (4, 5, 6);
+        ((g, h), i) = ((7, ((8))), ((((9)))));
+        ((((((j,),),),))) = 3;
+        (k, (((l, m, n),),), o, p, (q)) = (5, (6, 7, 8), 9, ((10,),), ((11,),),);
+
+    tel
+}
+
+fn main() {}
