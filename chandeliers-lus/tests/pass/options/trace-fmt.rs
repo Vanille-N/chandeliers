df1ef3b1f0e4855007b5f7b09b55fd774724bcfb
Neven Villani <vanille@crans.org>
2023-11-10 18:31:36 +0100

    some tests

---
+++ chandeliers-lus/tests/pass/options/trace-fmt.rs
@@ -0,0 +1,31 @@
+chandeliers_lus::decl! {
+    #[trace("Fizz")]
+    node fizz() returns ();
+    let tel;
+
+    #[trace("Buzz")]
+    node buzz() returns ();
+    let tel;
+
+    #[trace("{n}")]
+    node id(n : int) returns ();
+    let tel;
+
+    #[trace("\n")]
+    node newline() returns ();
+    let tel;
+
+    #[main(30)]
+    node main() returns ();
+    var n : int; div_3, div_5, div_either : bool;
+    let
+        n = 1 fby n + 1;
+        div_3 = (n % 3) = 0;
+        div_5 = (n % 5) = 0;
+        div_either = div_3 or div_5;
+        () = fizz(() when div_3);
+        () = buzz(() when div_5);
+        () = id(n whenot div_either);
+        () = newline();
+    tel;
+}
